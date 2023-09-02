3470805836fe2f7c3aebac97ce8e96b2a8bbf167
butkrit <yokart.butter@gmail.com>
2023-09-02 16:24:15 +0700

    done

--- src/app/page.js
+++ src/app/page.js
@@ -44,26 +44,39 @@ const schema = z
     }),
     hasCoupon: z.boolean(),
     coupon: z.string(),
-    password: z.string(),
-    confirmPassword: z.string(),
-  })
-  .refine(
-    //refine let you check error in your own way
-    //in this example, we check "hasCoupon" with "coupon" fields
-    (data) => {
-      // if user does not tick "I have coupon", then it's ok
-      if (!data.hasCoupon) return true;
-      // if user tick "I have coupon" and fill correct code, then it's ok too
-      if (data.hasCoupon && data.coupon === "CMU2023") return true;
-      // ticking "I have coupon" but fill wrong coupon code, show error
-      return false;
-    },
-    //set error message and the place it should show
-    {
-      message: "Invalid coupon code",
-      path: ["coupon"],
-    }
-  );
+    password: z
+    .string()
+    .max(12, { message: "Password must not exceed 12 characters" })
+    .min(6, { message: "Password must contain at least 6 characters" }),
+  confirmPassword: z.string(),
+})
+.refine(
+  (p) => {
+    if (p.password === p.confirmPassword) return true;
+    return false;
+  },
+  {
+    message: "Password does not match",
+    path: ["confirmPassword"],
+  }
+)
+.refine(
+  //refine let you check error in your own way
+  //in this example, we check "hasCoupon" with "coupon" fields
+  (data) => {
+    // if user does not tick "I have coupon", then it's ok
+    if (!data.hasCoupon) return true;
+    // if user tick "I have coupon" and fill correct code, then it's ok too
+    if (data.hasCoupon && data.coupon === "CMU2023") return true;
+    // ticking "I have coupon" but fill wrong coupon code, show error
+    return false;
+  },
+  //set error message and the place it should show
+  {
+    message: "Invalid coupon code",
+    path: ["coupon"],
+  }
+);
 
 export default function Home() {
   const [opened, { open, close }] = useDisclosure(false);
@@ -90,18 +103,22 @@ export default function Home() {
     //TIP : get value of currently filled form with variable "form.values"
 
     if (form.values.plan === "funrun") price = 500;
+    else if (form.values.plan === "mini") price = 800;
+    else if (form.values.plan === "half") price = 1200;
+    else if (form.values.plan === "full") price = 1500;
     //check the rest plans by yourself
     //TIP : check /src/app/libs/runningPlans.js
 
     //check discount here
-
+    if (form.values.hasCoupon && form.values.coupon === "CMU2023") price *= 0.7;
     return price;
+   
   };
 
   return (
     <div>
       <Container size="500px">
-        <Title italic align="center" color="pink">
+        <Title italic align="center" color="green">
           Register CMU Marathon 🥈
         </Title>
         <form onSubmit={form.onSubmit((v) => alert("See you at CMU Marathon"))}>
@@ -175,7 +192,7 @@ export default function Home() {
           </Stack>
         </form>
 
-        <Footer year={2023} fullName="Chayanin Suatap" studentId="650610560" />
+        <Footer year={2023} fullName="Thanakrit Yokart" studentId="620612150" />
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
